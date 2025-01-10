1b152119157bc85f89196dc6d284564174e13ee3
Fredchuks00 <chuksfredrick5@gmail.com>
2025-01-10 09:14:41 -0300

    Added tesseract OCR and started Writing code to access the image folder

--- OCR API/OCR API/Program.cs
+++ OCR API/OCR API/Program.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OCR_API
+
+
+
+namespace TextExtraction
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter path to your images ");
+            string inputFolder = Console.ReadLine();
+            List<Image> loadedImages = new List<Image>();
+
+            Console.WriteLine("loading images ..... ");
+            LoadImages(inputFolder, loadedImages);
+
+        }
+        public static void LoadImages(string path, List<Image> loadedImages)
         {
         }
     }
